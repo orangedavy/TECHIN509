18fa9b39fde1d57891beb97606fb120012f16cb4
Guo Shucheng <orangedavy@gmail.com>
2022-12-23 03:01:51 -0800

    added more test cases

--- Lab6/tests.py
+++ Lab6/tests.py
@@ -1,92 +1,129 @@
 import unittest
 import logic
+from unittest.mock import patch
 
 
 class TestLogic(unittest.TestCase):
 
-    def test_check_move (self):
-        board = [
-            ['X', None, 'O'],
-            [None, None, None],
-            [None, 'O', 'X'],
-        ]
-        game = logic.Game(logic.Human('X'), logic.Bot('O'))
-        self.assertEqual(logic.check_move(5, 'X', board), [
-            ['X', None, 'O'],
-            [None, 'X', None],
-            [None, 'O', 'X'],
-        ])
-
-        board = [
-            ['O', 'X', 'O'],
-            ['X', 'X', 'O'],
-            [None, 'O', 'X'],
-        ]
-        self.assertEqual(logic.check_move(7, 'X', board), [
-            ['O', 'X', 'O'],
-            ['X', 'X', 'O'],
-            ['X', 'O', 'X'],
-        ])
-
-        board = [
-            [None, None, None],
-            [None, None, None],
-            [None, None, None],
-        ]
-        self.assertEqual(logic.check_move(9, 'O', board), [
-            [None, None, None],
-            [None, None, None],
-            [None, None, 'O'],
-        ])
-
-        board = [
-            ['X', None, 'O'],
-            [None, None, None],
-            [None, 'O', 'X'],
-        ]
-
     def test_get_winner (self):
-        board = [
+        game = logic.Game(logic.Human('X'), logic.Bot('O'))
+
+        board_1 = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ]
+        board_2 = [
             ['X', None, 'O'],
             [None, 'X', None],
             [None, 'O', 'X'],
         ]
-        self.assertEqual(logic.get_winner(board), 'X')
-
-        board = [
+        board_3 = [
             ['X', 'O', 'O'],
-            ['O', 'X', 'X'],
-            ['X', 'O', 'O'],
-        ]
-        self.assertEqual(logic.get_winner(board), None)
-
-        board = [
-            ['X', None, None],
-            [None, None, None],
-            [None, 'O', None],
-        ]
-        self.assertEqual(logic.get_winner(board), None)
-
-        board = [
             ['X', 'X', 'X'],
-            ['O', 'X', 'O'],
             ['O', 'O', 'X'],
         ]
-        self.assertEqual(logic.get_winner(board), 'X')
-
-        board = [
-            [None, 'O', None],
-            [None, 'X', None],
-            [None, 'O', None],
+        board_4 = [
+            ['X', None, None],
+            [None, 'X', 'X'],
+            ['O', 'O', 'O'],
         ]
-        self.assertEqual(logic.get_winner(board), None)
+        board_5 = [
+            ['X', 'X', 'O'],
+            ['X', 'O', 'X'],
+            ['O', 'O', 'X'],
+        ]
+        board_6 = [
+            ['X', 'X', 'O'],
+            ['O', 'O', 'X'],
+            ['X', 'X', 'O'],
+        ]
+
+        game.board = board_1
+        self.assertEqual(game.get_winner(), None)
+        game.board = board_2
+        self.assertEqual(game.get_winner(), 'X')
+        game.board = board_3
+        self.assertEqual(game.get_winner(), 'X')
+        game.board = board_4
+        self.assertEqual(game.get_winner(), 'O')
+        game.board = board_5
+        self.assertEqual(game.get_winner(), 'O')
+        game.board = board_6
+        self.assertEqual(game.get_winner(), None)
 
     def test_switch_player (self):
-        player = 'X'
-        self.assertEqual(logic.switch_player(player), 'O')
+        game = logic.Game(logic.Human('X'), logic.Bot('O'))
 
-        player = 'O'
-        self.assertEqual(logic.switch_player(player), 'X')
+        self.assertEqual(game.player.symbol, 'X')
+
+        game.switch_player()
+        self.assertEqual(game.player.symbol, 'O')
+
+        game.switch_player()
+        self.assertEqual(game.player.symbol, 'X')
+
+    def test_update_board (self):
+        game = logic.Game(logic.Human('X'), logic.Bot('O'))
+
+        board_1 = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ]
+        board_2 = [
+            ['X', None, 'O'],
+            [None, None, None],
+            [None, 'O', 'X'],
+        ]
+
+        board_1_updated = [
+            [None, None, None],
+            [None, 'X', None],
+            [None, None, None],
+        ]
+        board_2_updated = [
+            ['X', 'X', 'O'],
+            [None, None, None],
+            [None, 'O', 'X'],
+        ]
+
+        game.board = board_1
+        game.update_board(1, 1)
+        self.assertEqual(game.board, board_1_updated)
+
+        game.board = board_2
+        game.update_board(0, 1)
+        self.assertEqual(game.board, board_2_updated)
+
+    def test_new_game (self):
+        game = logic.Game(logic.Human('X'), logic.Bot('O'))
+
+        game.resume = "Y"
+        game.new_game()
+        self.assertEqual(game.resume, False)
+
+        game.resume = "N"
+        with self.assertRaises(SystemExit) as new:
+            game.new_game()
+        self.assertEqual(new.exception.code, 'So long!')
+
+    def test_human_get_move (self):
+        human = logic.Human('X')
+        board = [
+            ['X', None, 'O'],
+            [None, None, None],
+            [None, 'O', 'X'],
+        ]
+
+        with patch('builtins.input', return_value='2'):
+            self.assertEqual(human.get_move(board), (0, 1))
+
+        with patch('builtins.input', return_value='5'):
+            self.assertEqual(human.get_move(board), (1, 1))
+
+        with patch('builtins.input', return_value='7'):
+            self.assertEqual(human.get_move(board), (2, 0))
 
 
 if __name__ == '__main__':
