03b8325791e42798195b5e76de5b11f06ac49030
Guo Shucheng <orangedavy@gmail.com>
2023-01-05 15:21:30 -0800

    optimized visual appearance

--- Lab10/server.py
+++ Lab10/server.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# renders index template for both original and home routes
 @app.route("/")
 @app.route("/home")
 def index():
     return render_template('Home.html')
 
+# renders play template for main game and extracts parameters from url
 @app.route("/play")
 def play():
     mode = request.args.get('mode')
@@ -15,11 +17,26 @@ def play():
     player_o = request.args.get('name_o')
     return render_template('Play.html', mode=mode, player_x=player_x, player_o=player_o)
 
+# renders html template for leaderboard
 @app.route("/stats")
 def stats():
     return render_template('Leaderboard.html')
 
+# redirects 400 error
+@app.errorhandler(400)
+def bad_request(e):
+    return redirect("/home")
+
+# redirects 404 error
+@app.errorhandler(404)
+def bad_request(e):
+    return redirect("/home")
+
+# redirects 500 error
+@app.errorhandler(500)
+def bad_request(e):
+    return redirect("/")
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5001)
+    app.debug = True  # debug mode
+    app.run(host='0.0.0.0', port=5001)  # different port
