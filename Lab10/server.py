c90ad6a22d0e38d9e3dfd83cb13750ba1e7438b3
Guo Shucheng <orangedavy@gmail.com>
2023-01-05 12:21:05 -0800

    big update to create fancier formatting

--- Lab10/server.py
+++ Lab10/server.py
@@ -1,4 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from logic import *
+from flask import Flask, render_template, request, redirect
+# from logic import *
 
 app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template('Home.html')
+
+@app.route("/play")
+def play():
+    mode = request.args.get('mode')
+    player_x = request.args.get('name_x')
+    player_o = request.args.get('name_o')
+    return render_template('Play.html', mode=mode, player_x=player_x, player_o=player_o)
+
+@app.route("/stats")
+def stats():
+    return render_template('Leaderboard.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5001)
